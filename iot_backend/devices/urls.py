37760b2ba95980f8ca67edc40f8172c78edc4f31
HiepNgo1609 <hiep.ngo1609@hcmut.edu.vn>
2023-04-03 08:51:02 +0700

    Done Insert SensorData and Update Sensors Value

--- iot_backend/devices/urls.py
+++ iot_backend/devices/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from . import views
+from .views import CreateSensorDataView, DevicesUpdateView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('sensors', views.SensorsViewSet)
-router.register('devices', views.DevicesViewSet)
+# router.register('devices', views.DevicesViewSet)
 router.register('deviceauto', views.DeviceAutoViewSet)
 router.register('sensordata', views.SensorDataViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('insertSensorData/', CreateSensorDataView.as_view(), name='insertSensorData'),
+    path('updateDevices/<int:pk>/v', DevicesUpdateView.as_view(), name='update-value')
 ]
--- iot_backend/iot_backend/settings.py
+++ iot_backend/iot_backend/settings.py
@@ -93,9 +93,9 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'iot',
+        'NAME': 'iot_1',
         'USER': 'root',
-        'PASSWORD': '123456789',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
